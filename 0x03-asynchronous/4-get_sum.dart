7f9915ca3dc502982ee4a83ed408a0cdb24ce787
Montywilliams <3650@holbertonschool.com>
2023-10-23 10:29:46 -0600

    correct return type to int

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,7 +1,7 @@
 import '4-util.dart';
 import 'dart:convert';
 
-Future<double> calculateTotal() async {
+Future<int> calculateTotal() async {
   try {
     var user = await fetchUserData();
     var userId = jsonDecode(user)['id'];
